bbab1bf55641cde624ad56a9c67bc948db49565f
Elrendar <guuto9@gmail.com>
2022-08-19 15:03:21 +0900

    Base Project

---
+++ src/main/java/com/sparta/carrot/Week7CarrotMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.carrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7CarrotMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7CarrotMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/carrot/Week7CarrotMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.carrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7CarrotMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
